452691832cadf59e25f58bb4a82311beb70fd344
Raimon Esteve <raimonesteve@gmail.com>
2025-01-16 12:36:53 +0100

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# This file is part stock_warehouse_search module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from .test_stock_warehouse_search import suite
+
+__all__ = ['suite']
