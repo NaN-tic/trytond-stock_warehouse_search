ad14cc53d64e49c2ce1a98ffaeecb01b88221954
Raimon Esteve <raimonesteve@gmail.com>
2026-06-15 08:10:03 +0200

    Restore warehouse as a Function field (replace by @property) From changeset 8aaf6e1eb5c1bfa277018d65ff8d780a153b2810

--- stock.py
+++ stock.py
@@ -1,15 +1,31 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
+from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.transaction import inactive_records
 
 
 class Location(metaclass=PoolMeta):
     __name__ = 'stock.location'
+    # Restore warehouse as a Function field in this module because stock
+    # replaced it with a property and search_warehouse requires a searcher.
+    warehouse = fields.Function(
+        fields.Many2One('stock.location', 'Warehouse'),
+        'get_warehouse', searcher='search_warehouse')
 
     @classmethod
     def __setup__(cls):
         super().__setup__()
-        cls.warehouse.searcher = 'search_warehouse'
+
+    def get_warehouse(self, name):
+        # Order by descending left to get the first one in the tree
+        with inactive_records():
+            locations = self.search([
+                    ('parent', 'parent_of', [self.id]),
+                    ('type', '=', 'warehouse'),
+                    ], order=[('left', 'DESC')])
+        if locations:
+            return locations[0].id
 
     @classmethod
     def search_warehouse(cls, name, clause):
